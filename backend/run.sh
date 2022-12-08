d4c42e9ac651b9ad7ea1141b69f05805a5e2c972
邹笑寒 <renovamenzxh@gmail.com>
2022-12-08 18:35:13 -0500

    fix backend script

--- backend/run.sh
+++ backend/run.sh
@@ -1,1 +1,1 @@
-cd src && python3 main.py --hostname 0.0.0.0 --port 80 --workers 3 --images 4 --delay 0 --loss 0
+cd src && sudo python3 main.py --hostname 0.0.0.0 --port 80 --workers 3 --images 4 --delay 0 --loss 0
